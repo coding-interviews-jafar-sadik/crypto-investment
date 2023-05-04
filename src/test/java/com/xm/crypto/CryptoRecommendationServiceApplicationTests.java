b57031a1dcdb7325c043d09e79295bb26b4e217e
dsadik <dzafar.sadik@gmail.com>
2023-05-04 15:26:24 +0200

    Initial commit: basic project structure

---
+++ src/test/java/com/xm/crypto/CryptoRecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xm.crypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoRecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
